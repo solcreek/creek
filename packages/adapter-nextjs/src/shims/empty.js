6d3674b2ee19b85d1cba490ca15c9a52cd1efb55
Yi-Ru Lin <deduce@gmail.com>
2026-04-06 19:28:50 -0500

    chore: remove adapter-nextjs from monorepo
    Moved to standalone repo: github.com/solcreek/adapter-creek

--- packages/adapter-nextjs/src/shims/empty.js
+++
@@ -1,2 +0,0 @@
-// Empty module shim — used to stub out unused dependencies
-export default {};
--- packages/adapter-nextjs/src/shims/env.js
+++
@@ -1,3 +0,0 @@
-// @next/env shim — env is already loaded at build time
-export function loadEnvConfig() {}
-export default { loadEnvConfig };
